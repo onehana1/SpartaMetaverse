606faa02d74123b6ad4582709efbbc5e3a1fa72c
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-02-18 18:07:38 +0900

    chore: Player.cs -> playerController.cs

--- Assets/Scripts/Entity/PlayerController.cs
+++ Assets/Scripts/Entity/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : BaseController
+public class PlayerController : BaseController
 {
     private Camera camera;
 
