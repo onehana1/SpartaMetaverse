5149c78905f03a3a8f8790ad2630efd86bde76d3
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-02-19 01:15:54 +0900

    feat: 미니게임 내 플레이어 생성 및 장애물 로직 구현 추가

---
+++ Assets/Scripts/MiniGame/Player.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Animator animator;
+    Rigidbody2D _rigidbody;
+
+    public float flapForce = 6f;
+    public float forwardSpeed = 3f;
+    public bool isDead = false;
+    float deathCooldown = 0f;
+
+    bool isFlap = false;
+
+    public bool godMode = false;
+
+    GameManager gameManager = null;
+
+    private void Start()
+    {
+        gameManager = GameManager.Instance;
+
+        animator = GetComponentInChildren<Animator>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+        if (animator == null)
+            Debug.LogError("NOT FOUNDED ANIMATOR");
+
+        if (_rigidbody == null)
+            Debug.LogError("NOT FOUNDED RIGIDBODY");
+    }
+
+    private void Update()
+    {
+        if (isDead)
+        {
+            if (deathCooldown <= 0)
+            {
+                if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+                {
+                    // 게임 재시작
+                    gameManager.RestartGame();
+                }
+            }
+            else
+            {
+                deathCooldown -= Time.deltaTime;
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+            {
+                isFlap = true;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isDead) return;
+
+        Vector3 velocity = _rigidbody.velocity;
+        velocity.x = forwardSpeed;
+
+        if (isFlap)
+        {
+            velocity.y += flapForce;
+            isFlap = false;
+        }
+
+        _rigidbody.velocity = velocity;
+
+        float angle = Mathf.Clamp((_rigidbody.velocity.y * 10f), -90, 90);
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (godMode) return;
+        if (isDead) return;
+
+        animator.SetInteger("isDead", 1);
+        isDead = true;
+        deathCooldown = 1f;
+        gameManager.GameOver();
+    }
+
+}
